0e19e016c3f7f922ebb6837fb20baf0db0e07f06
MiuLee <ccyprus@sina.cn>
2022-08-13 00:40:05 +0100

    load font

--- src/script.js
+++ src/script.js
@@ -1,1 +1,12 @@
 import './style.css'
+import font from 'three/examples/fonts/helvetiker_regular.typeface.json'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
+
+const fontLoader = new FontLoader()
+
+fontLoader.load(
+    'fonts/helvetiker_regular.typeface.json',
+    (font) => {
+        console.log('loaded')
+    }
+)
